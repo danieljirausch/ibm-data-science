cb2ff1b6825d9de9f53f920b26903e1c6c7014f6
danieljirausch <85104436+danieljirausch@users.noreply.github.com>
2024-02-02 22:58:43 +0100

    Add files via upload

--- 3-2-spacex_dash_app.py
+++ 3-2-spacex_dash_app.py
@@ -5,14 +5,14 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
+import math
+import numpy as np
 
 # Read the airline data into pandas dataframe
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
-print([min_payload, max_payload])        
-        
-
+launch_sites = ['CCAFS LC-40','CCAFS SLC-40','KSC LC-39A','VAFB SLC-4E']   
 
 # Create a dash application
 app = dash.Dash(__name__)
@@ -45,6 +45,9 @@ app.layout = html.Div(
         html.Div(dcc.Graph(id='success-pie-chart')),
         html.Br(),
 
+        html.Div(dcc.Graph(id='booster-bar-chart')),
+        html.Br(),
+
         html.P("Payload range (Kg):"),
         # TASK 3: Add a slider to select payload range
         #dcc.RangeSlider(id='payload-slider',...)
@@ -58,6 +61,7 @@ app.layout = html.Div(
 
         # TASK 4: Add a scatter chart to show the correlation between payload and launch success
         html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+
     ]
 )
 
@@ -73,7 +77,8 @@ def get_pie_chart(entered_site):
             all_df,
             values='class', 
             names='Launch Site', 
-            title='Total Success Launches By Site'
+            title='Total Success Launches By Site',
+            category_orders={"Launch Site": launch_sites}
         )
         return fig
     else:
@@ -84,7 +89,7 @@ def get_pie_chart(entered_site):
             counts_df, 
             values='count', 
             names='class', 
-            title=f'Total Success Launches for site {entered_site}'
+            title=f'Total Success Launches for site {entered_site}',
         )
         # return the outcomes piechart for a selected site
         return fig
@@ -106,7 +111,7 @@ def get_scatter_chart(entered_site, payload_range):
         scatter_df = filtered_df
         title = 'Correlation between Payload and Success for all Sites'
     else:
-        scatter_df = filtered_df[spacex_df['Launch Site']==entered_site]
+        scatter_df = filtered_df[filtered_df['Launch Site']==entered_site]
         title = f'Correlation between Payload and Success for site {entered_site}'
     fig = px.scatter(
         scatter_df,
@@ -114,6 +119,29 @@ def get_scatter_chart(entered_site, payload_range):
         y='class',
         color="Booster Version Category",
         title=title
+    )
+    return fig
+
+@app.callback(
+    Output(component_id='booster-bar-chart', component_property='figure'),
+    Input(component_id='site-dropdown', component_property='value'), 
+)
+def get_bar_chart(entered_site):
+    if entered_site == 'ALL':
+        bar_df = spacex_df.groupby(['Launch Site', 'Booster Version Category'])['class'].sum().reset_index()
+        title = 'Successful Launches by Booster Version'
+    else:
+        bar_df = spacex_df[spacex_df['Launch Site']==entered_site].groupby(['Launch Site', 'Booster Version Category'])['class'].sum().reset_index()
+        title = f'Correlation between Payload and Success for site {entered_site}'
+
+    bar_df = bar_df.rename(columns={'class': 'Successful Launches'})
+    fig = px.bar(
+        bar_df,
+        x='Booster Version Category',
+        y='Successful Launches',
+        color='Launch Site',
+        title=title,
+        category_orders={"Launch Site": launch_sites}
     )
     return fig
 
